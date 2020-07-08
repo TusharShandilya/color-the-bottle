1a6c34262834f8d668cb453dae5fb969e72123fa
TusharShandilya <tusharshandilya@live.com>
2020-07-08 22:02:41 +0530

    Updates

---
+++ precache-manifest.cbf48f1c8c3223c3edc025448f46980a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df6c235fa7c635f330b2858519a0654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f5a6c9c35f39482e6f7",
+    "url": "/static/css/main.6850501c.chunk.css"
+  },
+  {
+    "revision": "ff441cb146832dac5c79",
+    "url": "/static/js/2.5a87a546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a87a546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5a6c9c35f39482e6f7",
+    "url": "/static/js/main.25202259.chunk.js"
+  },
+  {
+    "revision": "dd0469f7125a07776ccb",
+    "url": "/static/js/runtime-main.76c1a280.js"
+  },
+  {
+    "revision": "7a3475005e895591b5630d36877761c4",
+    "url": "/static/media/logo.7a347500.svg"
+  }
+]);
